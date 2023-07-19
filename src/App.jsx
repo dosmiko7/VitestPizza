5c0f806fa30a83424b6bf004ae355cdf08b399e9
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-19 11:23:24 +0200

    feat: made a menu loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
 				path: "/",
 				element: <Home />,
 			},
-			{ path: "/menu", element: <Menu /> },
+			{ path: "/menu", element: <Menu />, loader: menuLoader },
 			{ path: "/cart", element: <Cart /> },
 			{ path: "/order/new", element: <CreateOrder /> },
 			{ path: "/order/:orderId", element: <Order /> },
