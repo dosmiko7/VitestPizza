d898a4da5ed89b0c23def55cc2f7084d0983d272
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 12:44:05 +0200

    fix: fixed Loader component positioning

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,9 +1,9 @@
 const Loader = () => {
 	return (
-		<div className="absolute bg-slate-200/20 inset-0 backdrop-blur-sm items-center justify-center">
-			<div className="loader"></div>
-		</div>
+	  <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+		<div className="loader"></div>
+	  </div>
 	);
-};
-
-export default Loader;
+  }
+  
+  export default Loader;
