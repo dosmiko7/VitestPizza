8f1efa675192413c9a3b9a65095c670f3dfe16d8
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 11:47:42 +0200

    feat: added style to Cart/CartItem and added additional type of Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,7 +5,8 @@ const Button = ({children, disabled, to, type}) => {
 
     const styles = {
         primary: base + " py-3 px-4 md:px-6 md:py-4",
-        small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs"
+        small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+        secondary: "uppercase border-2 border-stone-300 font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 hover:text-stone-800 focus:outline-none focus:ring focus:text-stone-800 focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5"
     }
 
     if(to) {
