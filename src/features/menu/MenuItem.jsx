8a6fa6377896e5e7cef6b2873d71401434c55ce8
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 11:34:22 +0200

    fix: align text in menu item

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({ pizza }) => {
 				alt={name}
 				className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
 			/>
-			<div className="flex flex-col grow">
+			<div className="flex flex-col grow pt-0.5">
 				<p className="font-medium">{name}</p>
 				<p className="text-sm italic text-stone-500 capitalize">{ingredients.join(", ")}</p>
 				<div className="mt-auto flex items-center justify-between">{!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
