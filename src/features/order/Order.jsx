9a796f46e502066a2b03de4c7d2531da9e64abda
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 18:30:00 +0200

    feat: made a possibility to change priority after the order

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -5,6 +5,7 @@ import { getOrder } from "../../services/apiRestaurant";
 import { calcMinutesLeft, formatCurrency, formatDate } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 const Order = () => {
 	const order = useLoaderData();
@@ -48,6 +49,8 @@ const Order = () => {
 				{priority && <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
 				<p className="font-bold">To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
 			</div>
+
+			{!priority && <UpdateOrder order={order}/>}
 		</div>
 	);
 }
