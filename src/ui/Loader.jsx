9b788d9eb05b23508b36c8212e20064db13747e4
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 11:16:19 +0200

    feat: added style to Loader and added custom styles to tailwind config

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,5 @@
 const Loader = () => {
-	return <div className="loader"></div>;
+	return <div className="absolute bg-slate-200/20 inset-0 backdrop-blur-sm items-center justify-center"><div className="loader"></div></div>;
 };
 
 export default Loader;
