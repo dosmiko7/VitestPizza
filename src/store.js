7d884fe11c1be79a91f7b9e5d4aae25f265403e2
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-20 15:49:05 +0200

    feat: cartSlice added

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./features/user/userSlice"
+import cartSlice from "./features/cart/cartSlice";
 
 const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        cart: cartSlice,
     }
 })
 
