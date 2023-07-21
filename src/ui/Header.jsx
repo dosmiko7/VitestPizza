686279752a88975390fffe872d277d8983eaf783
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-21 12:48:39 +0200

    fix: center items in header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 const Header = () => {
 	return (
-		<header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-500 sm:px-6 flex items-center justify-between ">
+		<header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-500 sm:px-6 grid grid-cols-3 justify-items-center">
 			<Link
 				to="/"
 				className="tracking-widest"
